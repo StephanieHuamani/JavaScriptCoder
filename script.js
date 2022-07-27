176a017a7d14a033c84028e4c06117d3abcf5200
StephanieHuamani <lsvh97@hotmail.com>
2022-07-27 18:58:08 -0300

    Simulador interactivo

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-let repetir = true
-
-
-while(repetir) {
-    let numero1 = parseFloat(prompt("Ingrese el precio del producto que quisiera comprar"))
-    let numero2 = 1.75
-    if(numero1){
-        alert(`El precio final que va a pagar es de: ${numero1 * numero2}`);
-    } else {
-        alert ("Ingrese un numero valido, por favor.")
-    }
-
-    let confirmacion = prompt ("¿Quiere calcular otro precio? (Si/No)").toLowerCase()
-    if(confirmacion == "no") {
-        repetir = false
-    }
+function descuento (numero, porcentaje){
+    return numero - (numero * porcentaje / 100)
 }
+
+function aumento (numero, porcentaje){
+    return numero + (numero * porcentaje / 100)
+}
+
+let numero, porcentaje
+
+do {
+    opcion = prompt ('¿Qué desea calcular?: \n\n 1)Calcular un descuento final de un producto \n 2)Calcular el aumento de un producto \n 3)Salir')
+    
+        do {
+            numero = parseFloat((prompt("Ingrese el precio del producto que quisiera comprar")));
+            porcentaje = parseInt(prompt("Ingrese el porcentaje"));
+        } while (isNaN(numero) || isNaN (porcentaje));
+    switch (opcion){
+        case '1':
+            descuento = alert(`El precio final que va a pagar es de: ${numero - (numero * porcentaje / 100)}`);
+            break
+        case '2':
+            aumento = alert(`El precio final que va a pagar es de: ${numero + (numero * porcentaje / 100)}`);
+            break
+        case '3':
+            alert('Hasta la próxima!')
+            break
+        default:
+            alert('Ingrese una un número valido, por favor')
+    }
+} while (opcion != '3');
+
+
+let resultado = descuento (numero,porcentaje)
+let resultado2 = aumento (numero, porcentaje)
+alert(`El precio final que va a pagar es de: ${numero - (numero * porcentaje / 100)}`);
+alert(`El precio final que va a pagar es de: ${numero + (numero * porcentaje / 100)}`)
+
+
