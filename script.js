9064a0c63bed8bc2d5ea4012c0d640c9a8ca2074
StephanieHuamani <lsvh97@hotmail.com>
2022-08-02 00:36:02 -0300

    Arrays

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-function descuento (numero, porcentaje){
-    return numero - (numero * porcentaje / 100)
-}
 
-function aumento (numero, porcentaje){
-    return numero + (numero * porcentaje / 100)
-}
-
-let numero, porcentaje
-
-do {
-    opcion = prompt ('¿Qué desea calcular?: \n\n 1)Calcular un descuento final de un producto \n 2)Calcular el aumento de un producto \n 3)Salir')
-    
-        do {
-            numero = parseFloat((prompt("Ingrese el precio del producto que quisiera comprar")));
-            porcentaje = parseInt(prompt("Ingrese el porcentaje"));
-        } while (isNaN(numero) || isNaN (porcentaje));
-    switch (opcion){
-        case '1':
-            descuento = alert(`El precio final que va a pagar es de: ${numero - (numero * porcentaje / 100)}`);
-            break
-        case '2':
-            aumento = alert(`El precio final que va a pagar es de: ${numero + (numero * porcentaje / 100)}`);
-            break
-        case '3':
-            alert('Hasta la próxima!')
-            break
-        default:
-            alert('Ingrese una un número valido, por favor')
+class Productos {
+    constructor(prenda, precio, stock, color) {
+        this.prenda = prenda
+        this.precio = precio
+        this.stock = stock
+        this.color = color
     }
-} while (opcion != '3');
+}
 
+const Productos1 = new Productos ("remera", 900, 5, "gris")
+const Productos2 = new Productos ("jean", 2900, 3, "negro")
+const Productos3 = new Productos ("buzo", 1800, 7, "bicolor")
 
-let resultado = descuento (numero,porcentaje)
-let resultado2 = aumento (numero, porcentaje)
-alert(`El precio final que va a pagar es de: ${numero - (numero * porcentaje / 100)}`);
-alert(`El precio final que va a pagar es de: ${numero + (numero * porcentaje / 100)}`)
+console.log(Productos)
 
+Productos.push("pollera", 1300, 4, "escoses")
 
+console.log(Productos)
