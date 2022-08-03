f817785a6bb0b09c35a8eeef4fb70c528e24477f
StephanieHuamani <lsvh97@hotmail.com>
2022-08-03 18:43:43 -0300

    Primer entrega del proyecto final

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-
-class Productos {
-    constructor(prenda, precio, stock, color) {
-        this.prenda = prenda
-        this.precio = precio
-        this.stock = stock
+class Prenda {
+    constructor(tipoDePrenda, nombre, talle, color, stock) {
+        this.tipoDePrenda = tipoDePrenda
+        this.nombre = nombre
+        this.talle = talle
         this.color = color
+        this.stock = stock
+    }
+    mostrarInfo() {
+        return `
+        Tipo de Prenda: <b>${this.tipoDePrenda}</b> </br>
+        Nombre: <b>${this.nombre}</b> </br>
+        Talle: <b>${this.talle}</b> </br>
+        Color: <b>${this.color}</b> </br>
+        Stock: <b>${this.stock}</b> </br>
+        `
     }
 }
 
-const Productos1 = new Productos ("remera", 900, 5, "gris")
-const Productos2 = new Productos ("jean", 2900, 3, "negro")
-const Productos3 = new Productos ("buzo", 1800, 7, "bicolor")
 
-console.log(Productos)
+const remeraS = new Prenda("Remera", "Sunshine", "S", "Negra", 6)
+const remeraM = new Prenda("Remera", "Sunshine", "M", "Negra", 5)
+const remeraL = new Prenda("Remera", "Sunshine", "L", "Negra", 3)
+const buzoS = new Prenda("Buzo", "Iman", "S", "Gris", 8)
+const buzoM = new Prenda("Buzo", "Iman", "M", "Gris", 4)
+const buzoL = new Prenda("Buzo", "Iman", "L", "Gris", 7)
+const buzoOversize = new Prenda("Buzo", "Covid-19", "Unico", "Negro", 10)
+const pantalon = new Prenda("Jean", "Plac", "2", "Beige", 15)
 
-Productos.push("pollera", 1300, 4, "escoses")
 
-console.log(Productos)
+let respuesta = prompt(`¿Qué stock quiere ver?`).toLocaleLowerCase()
+
+if (respuesta === "remera") {
+    document.write(`
+${remeraS.mostrarInfo()} <br>
+${remeraM.mostrarInfo()} <br>
+${remeraL.mostrarInfo()} <br><br>`)
+} else if (respuesta === "buzo") {
+    document.write(`
+    ${buzoS.mostrarInfo()} <br>
+    ${buzoM.mostrarInfo()} <br>
+    ${buzoL.mostrarInfo()} <br>
+    ${buzoOversize.mostrarInfo()} <br><br>`)
+} else if (respuesta === "pantalon") {
+    document.write(`
+        ${pantalon.mostrarInfo()} <br><br>`)
+} else{
+    alert('Prenda no existente y/o sin stock')
+}
